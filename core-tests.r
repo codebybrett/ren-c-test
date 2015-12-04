40082c42026e912816f413177d6b001834ee4d81
Hostile Fork <brian@hostilefork.com>
2015-12-04 05:46:10 -0500

    Call MODULE instead of MAKE MODULE!

--- core-tests.r
+++ core-tests.r
@@ -1420,30 +1420,24 @@
 ; bug#1930: Lookup crashes on empty hashed map.
 [m: make map! 8 clear m none? m/a]
 ; datatypes/module.r
-[module? make module! [[] []]]
+[module? module [] []]
 [not module? 1]
-[module! = type? make module! [[] []]]
+[module! = type? module [] []]
 [
-    a-module: make module! [
-        []
-        [
-            ; 'var will be in the module
-            var: 1
-        ]
+    a-module: module [
+    ][
+        ; 'var will be in the module
+        var: 1
     ]
     var: 2
     1 == a-module/var
 ]
 ; import test
 [
-    a-module: make module! [
-        [
-            exports: [var]
-        ]
-
-        [
-            var: 2
-        ]
+    a-module: module [
+        exports: [var]
+    ][
+        var: 2
     ]
     import a-module
     2 == var
@@ -1451,14 +1445,10 @@
 ; import test
 [
     var: 1
-    a-module: make module! [
-        [
-            exports: [var]
-        ]
-
-        [
-            var: 2
-        ]
+    a-module: module [
+        exports: [var]
+    ][
+        var: 2
     ]
     import a-module
     1 == var
