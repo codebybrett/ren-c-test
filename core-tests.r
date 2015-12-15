de091d1d4d3dca8bf52e9cd5cefa5d7bc9cce2b9
Andreas Bolka <a@bolka.at>
2015-12-15 04:26:12 +0100

    Change a newly merged DO + function! instance to use EVAL

--- core-tests.r
+++ core-tests.r
@@ -7006,7 +7006,7 @@
     error? switch 1 cases
 ]
 ; bug#2242
-[11 = do does [switch/all 1 [1 [return 11 88]] 99]]
+[11 = eval does [switch/all 1 [1 [return 11 88]] 99]]
 ; functions/control/throw.r
 ; see functions/control/catch.r for basic functionality
 ; the "result" of throw should not be assignable, bug#1515
