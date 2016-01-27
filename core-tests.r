c019d01158a350098620067772f073fee9524c3b
Hostile Fork <brian@hostilefork.com>
2016-01-27 08:16:36 -0500

    Add Trello VARARGS! examples as tests

--- core-tests.r
+++ core-tests.r
@@ -26,6 +26,43 @@
 ; bug#1659
 ; actions are active
 [1 == do reduce [:abs -1]]
+
+; better-than-nothing VARARGS! variadic argument tests
+
+[
+    foo: func [x [integer! <...>]] [
+        sum: 0
+        while [not tail? x] [
+            sum: sum + take x
+        ]
+    ]
+    y: (z: foo 1 2 3 | 4 5)
+    all [y = 5 | z = 6]
+]
+[
+    foo: func [x [integer! <...>]] [make block! x]
+    [1 2 3 4] = foo 1 2 3 4
+]
+[
+    alpha: func [x [integer! string! tag! <...>]] [
+        beta 1 2 (x) 3 4
+    ]
+    beta: func ['x [integer! string! word! <...>]] [
+        reverse (make block! x)
+    ]
+    all [
+        [4 3 "back" "wards" 2 1] = alpha "wards" "back"
+            |
+        error? trap [alpha <some> <thing>] ;-- both checks are applied in chain
+            |
+        [4 3 other thing 2 1] = alpha thing other
+    ]
+]
+[
+    ;-- leaked VARARGS! cannot be accessed after call is over
+    error? trap [take eval (foo: func [x [integer! <...>]] [x])]
+]
+
 ; datatypes/binary.r
 [binary? #{00}]
 [not binary? 1]
